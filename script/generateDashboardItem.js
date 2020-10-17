56f3d3a33b468cde1ae4ca9013689c8f686e7869
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2020-10-18 00:10:14 +0700

    dashboard added

---
+++ script/generateDashboardItem.js
@@ -0,0 +1,22 @@
+/**
+ * Generate dashboard item for dashboard page
+ * @param {Array.<{id:number, name:string, price:number, imagePath:string, quantitySold: number, currentQuantity:number}>} chocolateArray Array of listed chocolates
+ */
+const generateDashboardItem = (chocolateArray) => {
+  let items = "";
+  for (let i = 0; i < chocolateArray.length; i++) {
+    items += `
+    <div class="dashboard__item">
+      <img src="${chocolateArray[i].imagePath}" />
+      <div class="dashboard__item__description">
+        <div class="dashboard__item__description__title">${chocolateArray[i].name}</div>
+        <div>
+          <div class="dashboard__item__description__text">Amount sold: <span>${chocolateArray[i].quantitySold}</span></div>
+          <div class="dashboard__item__description__text">Price: Rp <span>${chocolateArray[i].currentQuantity}</span></div>
+        </div>
+      </div>
+    </div>
+    `;
+  }
+  document.getElementById("dashboard__itemcontainer").innerHTML = items;
+};
