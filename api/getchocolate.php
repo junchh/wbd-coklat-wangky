200340fe15752299c493cb01e20ae03d59d3aeaf
junchh <cjunho9868@gmail.com>
2020-10-21 18:50:48 +0700

    fix getchocolate

--- api/getchocolate.php
+++ api/getchocolate.php
@@ -2,34 +2,37 @@
 
 require_once 'config.php';
 
-if(true) {
-    $headers = getallheaders();
+if(isset($_GET['id'])) {
+    $user_id = isLoggedin($con);
+    if($user_id == -1){
+        $result = ["status" => "invalid_login", "description" => "Invalid login information!"];
+        exit(json_encode($result));   
+    }
 
-    print_r($headers);
-
-    // $id = $_GET['id'];
-
-    
-    // $query = $con->prepare("SELECT * FROM `chocolates` WHERE `id` = ?");
-    
-    // $query->bind_param("i", $id);
-    
-
-    // if(!$query->execute()){
-    //     $result = ["status" => "error", "description" => $con->error];
-    //     exit(json_encode($result));
-    // }
-
-    // $query_result = $query->get_result();
-    
-    // if($query_result->num_rows != 1) {
-    //     $result = ["status" => "auth_failed_nouser", "description" => "no such user!."];
-    //     exit(json_encode($result));
-    // }
-
-    // $userData = $query_result->fetch_assoc();
+    $id = $_GET['id'];
 
     
+    $query = $con->prepare("SELECT * FROM `chocolates` WHERE `id` = ?");
+    
+    $query->bind_param("i", $id);
+    
+
+    if(!$query->execute()){
+        $result = ["status" => "error", "description" => $con->error];
+        exit(json_encode($result));
+    }
+
+    $query_result = $query->get_result();
+    
+    if($query_result->num_rows != 1) {
+        $result = ["status" => "no_chocolate", "description" => "no such chocolate!."];
+        exit(json_encode($result));
+    }
+
+    $payload = $query_result->fetch_assoc();
+
+    $result = ["status" => "success", "description" => "retrieved chocolate.", "payload" => $payload];
+    echo json_encode($result); 
 
     
 
