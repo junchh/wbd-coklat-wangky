fc4d33a6a21c2aa360f6320f4e136883b961fd21
junchh <cjunho9868@gmail.com>
2020-10-21 17:11:53 +0700

    add login checking function

--- api/config.php
+++ api/config.php
@@ -15,7 +15,38 @@ if($con->connect_errno) {
 }
 
 function isLoggedin() {
+    $headers = getallheaders();
+    $token = $headers['sessiontoken'];
+
+    $query = $con->prepare("SELECT * FROM `sessions` WHERE `session_id` = ?");
+    $query->bind_param("s", $token);
+
+    if(!$query->execute()){
+        return false;
+    }
+
+    $query_result = $query->get_result();
+
+    if($query_result->num_rows == 0) {
+        return false;
+    }
+
+    $session_data = $query_result->fetch_assoc();
     
+    $expired = strtotime($session_data['expire_date']);
+    $now = time();
+    
+
+    if(!$query->execute()){
+        return false;
+    }
+
+    if($now > $expired) {
+        return false;
+    }
+
+    return true;
+
 }
 
 
--- api/getchocolate.php
+++ api/getchocolate.php
@@ -2,29 +2,32 @@
 
 require_once 'config.php';
 
-if(isset($_GET['id']) && isLoggedin()) {
+if(true) {
+    $headers = getallheaders();
 
-    $id = $_GET['id'];
+    print_r($headers);
+
+    // $id = $_GET['id'];
 
     
-    $query = $con->prepare("SELECT * FROM `chocolates` WHERE `id` = ?");
+    // $query = $con->prepare("SELECT * FROM `chocolates` WHERE `id` = ?");
     
-    $query->bind_param("i", $id);
+    // $query->bind_param("i", $id);
     
 
-    if(!$query->execute()){
-        $result = ["status" => "error", "description" => $con->error];
-        exit(json_encode($result));
-    }
+    // if(!$query->execute()){
+    //     $result = ["status" => "error", "description" => $con->error];
+    //     exit(json_encode($result));
+    // }
 
-    $query_result = $query->get_result();
+    // $query_result = $query->get_result();
     
-    if($query_result->num_rows != 1) {
-        $result = ["status" => "auth_failed_nouser", "description" => "no such user!."];
-        exit(json_encode($result));
-    }
+    // if($query_result->num_rows != 1) {
+    //     $result = ["status" => "auth_failed_nouser", "description" => "no such user!."];
+    //     exit(json_encode($result));
+    // }
 
-    $userData = $query_result->fetch_assoc();
+    // $userData = $query_result->fetch_assoc();
 
     
 
