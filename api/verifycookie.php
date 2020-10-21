49980e78f902ddeb768a3c74a5569649f66ae30f
junchh <cjunho9868@gmail.com>
2020-10-21 08:37:01 +0700

    add session checking API

---
+++ api/verifycookie.php
@@ -0,0 +1,49 @@
+<?php 
+
+require_once 'config.php';
+
+if(isset($_GET['sessiontoken'])) {
+
+    $token = $_GET['sessiontoken'];
+
+    $query = $con->prepare("SELECT * FROM `sessions` WHERE `session_id` = ?");
+    $query->bind_param("s", $token);
+
+    if(!$query->execute()){
+        $result = ["status" => "error", "description" => $con->error];
+        exit(json_encode($result));
+    }
+
+    $query_result = $query->get_result();
+
+    if($query_result->num_rows == 0) {
+        $result = ["status" => "no_session", "description" => "Session doesn't exist!"];
+        exit(json_encode($result));
+    }
+
+    $session_data = $query_result->fetch_assoc();
+    
+    $expired = strtotime($session_data['expire_date']);
+    $now = time();
+    
+
+    if(!$query->execute()){
+        $result = ["status" => "error", "description" => $con->error];
+        exit(json_encode($result));
+    }
+
+    if($now > $expired) {
+        $result = ["status" => "session_expired", "description" => "Session already expired!"];
+        exit(json_encode($result));
+    }
+
+    $payload = ["user_id" => $session_data['user_id']];
+    $result = ["status" => "success", "description" => "user successfully registered", "payload" => $payload];
+    echo json_encode($result); 
+
+    
+
+} else {
+    $result = ["status" => "error", "description" => "This method is not supported."];
+    exit(json_encode($result));
+}
