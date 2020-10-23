0897145f2097312f4a04fb058d119971344caff8
junchh <cjunho9868@gmail.com>
2020-10-23 21:05:11 +0700

    search working okay, no pagination yet

---
+++ api/search.php
@@ -0,0 +1,35 @@
+<?php 
+
+require_once 'config.php';
+
+if(isset($_GET['q'])){
+    $user_id = isLoggedin($con);
+    if($user_id == -1){
+        $result = ["status" => "invalid_login", "description" => "Invalid login information!"];
+        exit(json_encode($result));   
+    }
+
+    $q = "%{$_GET['q']}%";
+
+    $query = $con->prepare("SELECT * FROM `chocolates` WHERE `name` LIKE ? OR `description` LIKE ?");
+
+    $query->bind_param("ss", $q, $q);
+
+    if(!$query->execute()){
+        $result = ["status" => "error", "description" => $con->error];
+        exit(json_encode($result));
+    }
+
+    $query_result = $query->get_result();
+
+    $payload = [];
+    while($row = $query_result->fetch_assoc()) {
+        array_push($payload, transformRow($row));
+    }
+
+    $result = ["status" => "success", "description" => "retrieved all search results", "payload" => $payload];
+    echo json_encode($result); 
+} else {
+    $result = ["status" => "error", "description" => "This method is not supported."];
+    exit(json_encode($result));
+}
--- script/page/search/search.js
+++ script/page/search/search.js
@@ -1,11 +1,47 @@
 const origin = window.location.origin 
 const path = window.location.pathname 
 const query = new URL(window.location.href).searchParams
-console.log(query.get("q"))
+
+const callback = (response) => {
+    const resObj = JSON.parse(response)
+    if(resObj.status == "success") {
+        const searchResult = resObj.payload 
+
+        let items = ""
+        let len = searchResult.length 
+        for(let i = 0; i < len; i++) {
+            const item = searchResult[i]
+            items += `
+            <div class="search-items">
+                <div class="search-image">
+                    <a href="/detail.html?id=${item.id}"><img src="${item.imagePath}" /></a>
+                </div>
+                <div class="search-item-description">
+                    <h1><a href="/detail.html?id=${item.id}">${item.name}</a></h1>
+                    <h2>Amount sold: ${item.quantitySold}</h2>
+                    <h2>Price: ${parseInt(item.price).toLocaleString("id-ID")}</h2>
+                    <h2>Amount remaining: ${item.currentQuantity}</h2>
+                    <h2>Description</h2>
+                    <p>
+                        ${item.description}
+                    </p>
+                </div>
+            </div>
+            `
+        }
+        document.getElementById("search-result-container").innerHTML = items
+    }
+
+}
 
 searchBox = document.getElementById("search-query")
+searchBox.value = query.get('q')
+getAPI("/api/search.php?q=" + searchBox.value, callback)
+
 searchBox.addEventListener('input', () => {
 
     query.set('q', searchBox.value)
     window.history.replaceState(null, '', origin + path + '?q=' + query.get('q'))
+    
+    getAPI("/api/search.php?q=" + searchBox.value, callback)
 })
